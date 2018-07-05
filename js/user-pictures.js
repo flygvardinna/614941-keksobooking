bb35742d9d80865d2e325165319218f42c95cebd
Оксана <zhuk11oxana@gmail.com>
2018-07-05 20:28:47 +0700

    правки по стилю

--- js/user-pictures.js
+++ js/user-pictures.js
@@ -3,62 +3,62 @@
 (function () {
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
- var avatarFileChooser = document.querySelector('#avatar');
- var avatarPreview = document.querySelector('.ad-form-header__user-pic');
- var flatPhotosContainer = document.querySelector('.ad-form__photo-container');
- var imagesFileChooser = flatPhotosContainer.querySelector('#images');
- var imageContainer = flatPhotosContainer.querySelector('.ad-form__photo');
- var imagePreview;
+  var avatarFileChooser = document.querySelector('#avatar');
+  var avatarPreview = document.querySelector('.ad-form-header__user-pic');
+  var flatPhotosContainer = document.querySelector('.ad-form__photo-container');
+  var imagesFileChooser = flatPhotosContainer.querySelector('#images');
+  var imageContainer = flatPhotosContainer.querySelector('.ad-form__photo');
+  var imagePreview;
 
- var addPhoto = function (photos) {
-   var fragment = document.createDocumentFragment();
-   var newImageContainer = imageContainer.cloneNode(true);
-   imagePreview = newImageContainer.querySelector('.ad-form__housing-pic');
-   fragment.appendChild(newImageContainer);
-   return fragment;
- };
+  var addPhoto = function () {
+    var fragment = document.createDocumentFragment();
+    var newImageContainer = imageContainer.cloneNode(true);
+    imagePreview = newImageContainer.querySelector('.ad-form__housing-pic');
+    fragment.appendChild(newImageContainer);
+    return fragment;
+  };
 
- avatarFileChooser.addEventListener('change', function () {
-   var file = avatarFileChooser.files[0];
-   var fileName = file.name.toLowerCase();
+  avatarFileChooser.addEventListener('change', function () {
+    var file = avatarFileChooser.files[0];
+    var fileName = file.name.toLowerCase();
 
-   var matches = FILE_TYPES.some(function (it) {
-    return fileName.endsWith(it);
-   });
+    var matches = FILE_TYPES.some(function (it) {
+     return fileName.endsWith(it);
+    });
 
-   if (matches) {
-     var reader = new FileReader();
+    if (matches) {
+      var reader = new FileReader();
 
-     reader.addEventListener('load', function () {
-       avatarPreview.src = reader.result;
-     });
+      reader.addEventListener('load', function () {
+        avatarPreview.src = reader.result;
+      });
 
-     reader.readAsDataURL(file);
-   }
- });
+      reader.readAsDataURL(file);
+    }
+  });
 
- imagesFileChooser.addEventListener('change', function () {
-   var file = imagesFileChooser.files[0];
-   var fileName = file.name.toLowerCase();
+  imagesFileChooser.addEventListener('change', function () {
+    var file = imagesFileChooser.files[0];
+    var fileName = file.name.toLowerCase();
 
-   var matches = FILE_TYPES.some(function (it) {
-    return fileName.endsWith(it);
-   });
-   if (matches) {
-     var reader = new FileReader();
+    var matches = FILE_TYPES.some(function (it) {
+     return fileName.endsWith(it);
+    });
+    if (matches) {
+      var reader = new FileReader();
 
-     reader.addEventListener('load', function () {
-       if (!imagePreview) {
-         imageContainer.innerHTML = '<img class= "ad-form__housing-pic" src="" alt="Фотография жилья" width="40" height="44">';
-         imagePreview = imageContainer.querySelector('.ad-form__housing-pic');
-         imagePreview.src = reader.result;
-       } else {
-         flatPhotosContainer.appendChild(addPhoto(file));
-         imagePreview.src = reader.result;
-       }
-     });
+      reader.addEventListener('load', function () {
+        if (!imagePreview) {
+          imageContainer.innerHTML = '<img class= "ad-form__housing-pic" src="" alt="Фотография жилья" width="40" height="44">';
+          imagePreview = imageContainer.querySelector('.ad-form__housing-pic');
+          imagePreview.src = reader.result;
+        } else {
+          flatPhotosContainer.appendChild(addPhoto());
+          imagePreview.src = reader.result;
+        }
+      });
 
-     reader.readAsDataURL(file);
-   }
- });
+      reader.readAsDataURL(file);
+    }
+  });
 })();
